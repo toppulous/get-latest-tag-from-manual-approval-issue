99d9dd3516279f1a088b9bd89c68bc9987da990a
nrtopping <nrtopping@gmail.com>
2020-05-21 01:33:27 -0700

    Initial processing of comments

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const github = require('@actions/github');
 async function run() {
   const issue_number = core.getInput('issue-number');
   const token = core.getInput('github-token');
+  const keyword = core.getInput('keyword');
 
   core.info(`Getting latest tag off of ${issue_number}`);
 
@@ -14,8 +15,16 @@ async function run() {
     issue_number: issue_number,
   });
 
+  processed_comments = comments.map(comment => {
+    user_login: comment.user.login,
+    body: comment.body,
+    created_at: comment.created_at,
+  }).filter(comment => comment.user_login.includes('github-actions'))
+  .filter(comment => body.startsWith(keyword))
+  .sort((a,b) => new Date(b.created_at) - new Date(a.created_at));
+
   core.info('Got comments');
-  console.log(comments);
+  console.log(processed_comments);
 
   core.setOutput('tag', 'tag'); // TODO
 }
