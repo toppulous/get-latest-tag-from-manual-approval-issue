f58059771b025101f7e95557be85741264095255
nrtopping <nrtopping@gmail.com>
2020-05-21 01:13:30 -0700

    Initial action setup, seeing how comments api works

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  const issue_number = core.getInput('issue-number');
+  const token = core.getInput('github-token');
+
+  core.info(`Getting latest tag off of ${issue_number}`);
+
+  const octokit = new github.GitHub(token);
+  const { data: comments } = await octokit.issues.listComments({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: issue_number,
+  });
+
+  core.info('Got comments');
+  core.info(comments);
+
+  core.setOutput('tag', 'tag'); // TODO
+}
+
+core.info('Starting...');
+run();
